c6a2679dc1b309c7994e8d9977bad30a673e7930
lokesh2201013 <lokeshchoraria2@gmail.com>
2024-02-07 23:48:32 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,15 +4,25 @@ import {styles} from '../styles'
 import { navLink } from '../constants'
 import { logo,menu,close } from '../assets'
 const Navbar = () => {
-  return (
-    <nav className={
-      `${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`
-    }>
-     <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
-      
-     </div>
-    </nav>
-  )
-}
+  const [active, setActive] = useState('');
 
-export default Navbar
+  // Function to handle link clicks
+  const handleLinkClick = () => {
+    setActive(" ");
+    window.scrollTo(0, 0);
+  };
+
+  return (
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        <Link to='/' className={`flex items-center gap-2 ${active === ' ' ? 'active' : ''}`} onClick={handleLinkClick}>
+          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+          <p className='text-white text-[18px] font-bold cursor-pointer'>Lokesh<span>|Anime lover</span></p>
+        </Link>
+        
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
