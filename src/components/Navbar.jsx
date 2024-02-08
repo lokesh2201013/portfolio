9360fdd56aa6951a4974a1a03f97fca06037a0bb
lokesh2201013 <lokeshchoraria2@gmail.com>
2024-02-08 23:08:17 +0530

    navabar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,31 @@
-import React,{useEffect,useState} from 'react'
-import { Link } from 'react-router-dom'
-import {styles} from '../styles'
-import { navLink } from '../constants'
-import { logo,menu,close } from '../assets'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { styles } from '../styles';
+import { navLink } from '../constants';
+import { logo, menu, close } from '../assets';
+
 const Navbar = () => {
   const [active, setActive] = useState('');
 
-  // Function to handle link clicks
   const handleLinkClick = () => {
-    setActive(" ");
+    setActive('');
     window.scrollTo(0, 0);
   };
 
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
-        <Link to='/' className={`flex items-center gap-2 ${active === ' ' ? 'active' : ''}`} onClick={handleLinkClick}>
+        <Link to='/' className={`flex items-center gap-2 ${active === '' ? 'active' : ''}`} onClick={handleLinkClick}>
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
-          <p className='text-white text-[18px] font-bold cursor-pointer'>Lokesh<span>|Anime lover</span></p>
+          <p className='text-white text-[18px] font-bold cursor-pointer'>Lokesh<span className='sm:block hidden'>|Anime lover</span></p>
         </Link>
-        
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLink.map((link) => (
+            <li key={link.id}>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   );
